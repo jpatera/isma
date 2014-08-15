285404626c610007515a29c52f8297832f494460
jan patera <jan.patera@gmail.com>
2014-08-15 17:06:12 +0200

    update

---
+++ src/main/java/eu/japtor/isma/model/CommentRepo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eu.japtor.isma.model;
+
+import java.util.List;
+
+/**
+ *
+ * @author Honza
+ */
+public interface CommentRepo {
+    
+//    public List<Comment> getCommentsForIssue(Issue issue);
+//    public Comment getCommentById(Long id);
+////    public String createComment(Comment comment);
+////    public int updateComment(Comment comment);
+////    public int deleteCommentById(Long id);
+    
+}
